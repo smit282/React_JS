1b4d5cffcc60aaed0f0f83d487976b85e95aafab
Smit Thummar <smitthummar28@gmail.com>
2024-04-14 23:19:52 +0530

    destructuring

---
+++ lec-8/myapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Destructuring from './components/Destructuring';
+
+function App() {
+  return (
+    <div className="App">
+      <Destructuring/>
+    </div>
+  );
+}
+
+export default App;
---
+++ lec-8/myapp/src/components/Destructuring.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Destructuring = props => {
+  return (
+    // without Destructuring
+    // <div> 
+    //     <div className="XYZ"> 
+    //         <h3> {props.active} </h3> 
+    //     </div> 
+  
+    //     <div className="PQR"> 
+    //         <h1>{props.activeStatus}</h1> 
+    //     </div> 
+    // </div> 
+
+    // with Destructuring 
+    <div> 
+        <div className="XYZ"> 
+            <h3> {active} </h3> 
+        </div> 
+  
+        <div className="PQR"> 
+            <h1>{activeStatus}</h1> 
+        </div>
+    </div>
+  )
+}
+
+export default Destructuring
