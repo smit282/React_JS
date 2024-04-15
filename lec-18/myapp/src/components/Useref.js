c14eaf096097da23d2cf715cc8743c3e4e8b9724
Smit Thummar <smitthummar28@gmail.com>
2024-04-15 08:30:13 +0530

    useref

---
+++ lec-18/myapp/src/components/Useref.js
@@ -0,0 +1,22 @@
+import './App.css';
+import React, {useRef} from "react"
+
+function App() {
+    const ref = useRef();
+    console.log(ref, "ref");
+
+    const handleRef = () => {
+        // ref.current.focus();
+        ref.current.style.color="red";
+        // ref.current.style.display="none";
+    }
+
+    return (
+        <div className="App">
+            <input type="text" ref={ref} />
+            <button onClick={handleRef}>Click Me</button>
+        </div>
+    );
+}
+
+export default App;
