9a07182936856373e5e25e78e9fb90fab263268f
Smit Thummar <smitthummar28@gmail.com>
2024-04-15 09:04:54 +0530

    incre-dicre demo

---
+++ lec-11/myapp/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react'
+import Incre from './components/Incre';
+
+function App() {
+  return (
+    <div className='App'>
+      <Incre/>
+    </div>
+  )
+}
+
+export default App
