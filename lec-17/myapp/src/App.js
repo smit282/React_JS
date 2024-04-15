ee2a7d951354130ef083c235e4b7b951cfe3118b
Smit Thummar <smitthummar28@gmail.com>
2024-04-15 08:29:30 +0530

    MUI table

---
+++ lec-17/myapp/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+// import './App.css';
+import CustomizedTables from './components/Table-MUI';
+
+function App() {
+  return (
+    <div className="App">
+      <CustomizedTables/>
+    </div>
+  );
+}
+
+export default App;
